141dc74105cd0ea1a9a5ad173845d4eebc0ec183
voidif <529399971@qq.com>
2018-12-04 00:08:17 -0800

    0.091
    Add backend url parse

--- src/GetResponse.java
+++ src/GetResponse.java
@@ -3,6 +3,7 @@ import org.json.JSONObject;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 
 public class GetResponse {
@@ -32,19 +33,18 @@ public class GetResponse {
 
             //parseURL
             String url = parseURL(head);
+            //System.out.println(url);
             //return index html or rate json
-            if(url.equals("/")){
-                writeHTML(response, url);
-            } else if(!url.substring(0,2).equals("/f")){
+            if(url.length() > 1 && url.charAt(1) == '?'){
                 //get rate from database
                 float rate = DataBaseConnection.getRate("USD_CNY");
                 JSONObject rateJson = new JSONObject();
                 rateJson.put("USD_CNY", rate);
                 //response
                 writeJson(response, rateJson);
+            } else {
+                writeFile(response, url);
             }
-
-
             response.flush();
             response.close();
         } catch (Exception e) {
@@ -79,6 +79,7 @@ public class GetResponse {
      * @param response The response that the file will write in.
      * @param url the url of the HTML file.
      */
+    @Deprecated
     public void writeHTML(OutputStream response, String url) throws IOException{
         //read HTML
         url = "webpage" + url + "index.html";
@@ -107,6 +108,49 @@ public class GetResponse {
                 "Content-Type: application/json" + "\r\n" + "\r\n";
         response.write(head.getBytes());
         response.write(json.toString().getBytes());
+    }
+
+    /**
+     * Return a specific file to client according the URL.
+     * @param response The response that the file will write in.
+     * @param url The url that points to a file.
+     */
+    private void writeFile(OutputStream response, String url) throws IOException{
+        System.out.println(url);
+        if(url.equals("/")) {url = url + "index.html";}
+        //read file
+        url = "webpage" + url;
+        String htmlPath = this.getClass().getResource(url).getPath();
+        File file = new File(htmlPath);
+        FileInputStream fis = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fis.read(data);
+        fis.close();
+        //get file extension and convert it to lower case letter
+        String[] tmp = url.split("\\.");
+        char[] extArray = tmp[tmp.length - 1].toCharArray();
+        for(int i = 0; i < extArray.length; i ++){
+            if(extArray[i] <= 'Z' && extArray[i] >= 'A'){
+                extArray[i] = (char)(extArray[i] - ('A' - 'a'));
+            }
+        }
+        String ext = new String(extArray);
+        //make HTTP Head
+        StringBuilder head = new StringBuilder("HTTP/1.1 200 OK" + "\r\n");
+        head.append("content-length: " + data.length + "\r\n");
+        //looking the file extension
+        if(ext.equals("html")){
+            head.append("Content-Type: text/html; charset=utf-8" + "\r\n" + "\r\n");
+        } else if(ext.equals("css")){
+            head.append("content-type: text/css; charset=utf-8" + "\r\n" + "\r\n");
+        } else if(ext.equals("jpg")){
+            head.append("Content-Type: image/png" + "\r\n" + "\r\n");
+        } else if(ext.equals("png")){
+            head.append("Content-Type: image/jpeg" + "\r\n" + "\r\n");
+        }
+        //write into response
+        response.write(head.toString().getBytes());
+        response.write(data);
     }
 
 //    public String getParasFromURL(URL url, String key){
