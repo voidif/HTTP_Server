8dc1b2326e885a750a0347378860fa5545d8d3d1
voidif <529399971@qq.com>
2019-01-22 23:41:03 -0800

    Update RedisTest.java

--- test/RedisTest.java
+++ test/RedisTest.java
@@ -6,8 +6,6 @@ public class RedisTest {
 
     public static void main(String[] args) {
         Jedis jedis = new Jedis(URL);
-        System.out.println("连接成功");
-        //查看服务是否运行
-        System.out.println("服务正在运行: "+jedis.ping());
+        System.out.println(jedis.get("test"));
     }
 }
