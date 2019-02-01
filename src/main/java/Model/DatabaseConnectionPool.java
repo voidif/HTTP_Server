28e0c4958551bae631d63864b06b00d7db1bc441
voidif <529399971@qq.com>
2019-01-31 21:56:37 -0800

    Update DatabaseConnectionPool.java

--- src/main/java/Model/DatabaseConnectionPool.java
+++ src/main/java/Model/DatabaseConnectionPool.java
@@ -1,5 +1,7 @@
 package Model;
 
+import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -217,6 +219,8 @@ class pooledConnection{
             //close connection
             closeConnection();
             return false;
+        } catch(CommunicationsException e) {
+
         }
         return true;
     }
